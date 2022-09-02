fd3f85525c6c12e89b9286de142dd2bace53f227
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-09-03 01:54:57 +0600

    sorting section added

--- js/news.js
+++ js/news.js
@@ -1,20 +1,59 @@
-
+//----------first take all the categories -------------//
 const url = 'https://openapi.programming-hero.com/api/news/categories'
 fetch(url)
     .then(res => res.json())
     .then(data => displayNewsCatagories(data.data.news_category))
     .catch(error => console.log(error))
-
+//---------------daynamically add the catagories name------------//
 const displayNewsCatagories = catagories => {
     const parentField = document.getElementById('news-catagories-field');
-    catagories.forEach(catagory => {
-        console.log(catagory)
+    catagories.forEach(category => {
+        // console.log(category)
         const catagoriesField = document.createElement('div');
-        // catagoriesField.classList.add('btn,btn-outline-danger');
         catagoriesField.innerHTML = `
-        <button class = "btn btn-outline-dark" onclick="catagoriesClick()"> ${catagory.category_name}</button>
+        <button class = "btn btn-outline-dark mx-auto" onclick="catagoriesClick(${category.category_id},'${category.category_name}')"> ${category.category_name}</button>
         `
         parentField.appendChild(catagoriesField);
+      
     })
 
 }
+// --------------spinner--------------/
+const toggleSpinner = isLoading =>{
+   const spinnerField =  document.getElementById('spinner');
+   if(isLoading === true){
+    spinnerField.classList.remove('d-none')
+   }
+   else{
+    spinnerField.classList.add('d-none')
+   }
+};
+
+
+// ----------------catagories onclick funtion two dynamic parameter--------------//
+const catagoriesClick = (id, name) => {
+    toggleSpinner(true)
+    const url = `https://openapi.programming-hero.com/api/news/category/0${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayNews(data.data, name))
+        .catch(error => console.log(error));
+
+}
+const displayNews = (articles, name) => {
+    toggleSpinner(false)
+    const sortField = document.getElementById('sort-section')
+    const countField = document.getElementById('count-field');
+    countField.innerHTML = `
+    <h5 class="fw-normal border bg-white px-4 py-2 my-4">${articles.length} items found for category <span class="text-danger">${name}</span></h5>
+    `
+    sortField.classList.remove('d-none')
+
+    articles.forEach(article =>{
+        const newsParentDiv = document.getElementById('news-field');
+        newsParentDiv.innerHTML = '';
+        const newsChildDiv = document.createElement('div');
+        newsChildDiv.classList.add
+    })
+}
+
