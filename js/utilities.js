a13e7397072ded661830b6df589916f8f54cbb91
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-09-03 22:46:19 +0600

    arrow funtion add and done

--- js/utilities.js
+++ js/utilities.js
@@ -1,7 +1,6 @@
-
-function blogFunc(){
+const  blogFunc =() => {
     window.location.href = 'http://127.0.0.1:5500/blog.html';
 }
-function newsFunc(){
+const  newsFunc =() => {
     window.location.href = 'http://127.0.0.1:5500/index.html';
 }
