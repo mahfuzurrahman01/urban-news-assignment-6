63cda50684329a4b43823b7d7137028a065d1301
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-09-04 00:35:48 +0600

    check again

--- js/utilities.js
+++
@@ -1,6 +0,0 @@
-// const  blogFunc = () => {
-//     window.location.href = 'http://127.0.0.1:5500/another.html';
-// }
-// const  newsFunc = () => {
-//     window.location.href = 'http://127.0.0.1:5500/index.html';
-// }
