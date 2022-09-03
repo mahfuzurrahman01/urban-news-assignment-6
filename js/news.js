50bba049c637a0334f8392efcfdf28511fd82771
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-09-03 19:42:59 +0600

    modal design done

--- js/news.js
+++ js/news.js
@@ -32,7 +32,7 @@ const toggleSpinner = isLoading => {
 
 // ----------------catagories onclick funtion two dynamic parameter--------------//
 const catagoriesClick = (id, name) => {
-   
+
     toggleSpinner(true)
     const url = `https://openapi.programming-hero.com/api/news/category/0${id}`
     fetch(url)
@@ -60,12 +60,9 @@ const displayNews = (articles, name) => {
     sortField.classList.remove('d-none')
     const newsParentDiv = document.getElementById('news-field');
     newsParentDiv.innerHTML = '';
-
+    // sorting the array 
+    articles.sort((a, b) => b.total_view - a.total_view);
     articles.forEach(article => {
-        const view = article.total_view
-        articles.sort((a,b) => {
-            return b.view - a.view;
-        })
         const newsChildDiv = document.createElement('div');
         newsChildDiv.classList.add('card', 'mb-3', 'p-3')
         newsChildDiv.innerHTML = `
@@ -79,20 +76,20 @@ const displayNews = (articles, name) => {
                 <p class="card-text">${article.details.slice(0, 300)}...</p>
                 <div class="card-text d-flex justify-content-between text-secondary">
                     <div class='d-flex align-items-center gap-1'>
-                        <img src='${article.author.img}' class='author-img'>
+                        <img src='${article.author.img}' class='author-img border border-3 border-danger'>
                         <div>
                             <p class="m-0 small">${article.author.name ? article.author.name : 'Authors not found'}</p>
                             <p class="m-0 small">${article.author.published_date ? article.author.published_date : 'Publish date not found'}</p>
                         </div>
                     </div>
-                    <!-- ------------------2---------------- -->
+                   
                     <div class='d-flex align-items-center gap-1'>
                         <i class="fa-regular fa-eye"></i>
                         <div>
                             <small>${article.total_view ? article.total_view : 'No data found'}</small>
                         </div>
                     </div>
-                    <!-- -----------------------3------------------ -->
+                   
                     <div class='d-flex align-items-center'>
                         <div>
                             <i class="fa-sharp fa-solid fa-star"></i>
@@ -102,7 +99,7 @@ const displayNews = (articles, name) => {
                             <i class="fa-solid fa-star-half-stroke"></i>
                         </div>
                     </div>
-                    <!-- --------------------4----------------------------- -->
+                
                     <div>
                         <button class="btn btn-danger" onclick="detailsBtn('${article._id}')" data-bs-toggle="modal" data-bs-target="#exampleModal">Details</button>
                     </div>
@@ -116,7 +113,7 @@ const displayNews = (articles, name) => {
 }
 
 const detailsBtn = id => {
-    
+
     const url = `https://openapi.programming-hero.com/api/news/${id}`;
     fetch(url)
         .then(res => res.json())
@@ -129,12 +126,16 @@ const displayModal = info => {
     const modalField = document.getElementById('modal-body');
     modalField.innerHTML = '';
     modalField.innerHTML = `
-    <img src ='${info.thumbnail_url}' class ='img-fluid'>
-    <h4>${info.title}</h4>
-    <h6>Author:<span class='text-danger'> ${info.author.name ? info.author.name : 'Author not found'}</span></h6>
-    <h6>Publish:<span class='text-danger'> ${info.author.published_date ? info.author.published_date : 'Publish date not found'}</span></h6>
-    <h6>Badge:<span class='text-danger'> ${info.rating.badge ? info.rating.badge : 'Badge not found'}</span></h6>
+    <img src ='${info.image_url}' class ='img-fluid border border-2 rounded'>
+    <img src = '${info.author.img}' class ='modal-author-img border border-3 border-danger'>
+   <div class='modal-article'>
+   <h6><span class='text-danger text-center'> ${info.author.name ? info.author.name : 'Author not found'}</span></h6>
+   <h6>Publish:<span class='text-danger'> ${info.author.published_date ? info.author.published_date : 'Publish date not found'}</span></h6>
+   <h6>Badge:<span class='text-danger'> ${info.rating.badge ? info.rating.badge : 'Badge not found'}</span></h6>
+   <p>${info.details.slice(0, 300)}...</p>
+   </div>
+    
     `
 }
 
-catagoriesClick(08,'All News')
+catagoriesClick(08, 'All News')
