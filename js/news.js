ee18375713efd10ea7abfc2da4d52caf276b1e14
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-09-03 21:57:40 +0600

    blog section location add

--- js/news.js
+++ js/news.js
@@ -144,3 +144,6 @@ const displayModal = info => {
 }
 
 catagoriesClick(08, 'All News')
+
+
+
---
+++ js/utilities.js
@@ -0,0 +1,7 @@
+
+function blogFunc(){
+    window.location.href = 'http://127.0.0.1:5500/blog.html';
+}
+function newsFunc(){
+    window.location.href = 'http://127.0.0.1:5500/index.html';
+}
