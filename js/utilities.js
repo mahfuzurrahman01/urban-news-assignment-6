1cb89f48a64e99c50e1746920d9aa02a32b02069
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-09-03 23:10:12 +0600

    done

--- js/utilities.js
+++ js/utilities.js
@@ -1,6 +1,6 @@
-const  blogFunc =() => {
+const  blogFunc = () => {
     window.location.href = 'http://127.0.0.1:5500/blog.html';
 }
-const  newsFunc =() => {
+const  newsFunc = () => {
     window.location.href = 'http://127.0.0.1:5500/index.html';
 }
